f388ea462b04298ea3785c3d09999963fad52fbd
brucelee03 <rohit.prajapati25603@gmail.com>
2024-06-12 14:55:36 +0000

    RactJs Intro: Practice

---
+++ index.js
@@ -0,0 +1,8 @@
+const className = 'greeting'
+const element = (
+  <div>
+    <h1 className={className}>Hello Rohit</h1>
+    <p>I am the world's no one hacker in the world</p>
+  </div>
+)
+ReactDOM.render(element, document.getElementById('root'))
